5b0b392207658c24b7d85879f61a48e4c93feb0c
Bart Van Bos <bartvanbos@gmail.com>
2021-02-26 18:11:32 +0100

    add workload scripts using k6

--- workloads/httpbin/load-scripts/500-InternalServerError.js
+++ workloads/httpbin/load-scripts/500-InternalServerError.js
@@ -1,5 +1,12 @@
 import http from "k6/http";
 
+export let options = {
+  vus: 5,
+  stages: [
+      { duration: "5m", target: 50 }
+  ]
+};
+
 export default function() {
     let response = http.get("http://httpbin.aspendemo.org/status/500");
 };
