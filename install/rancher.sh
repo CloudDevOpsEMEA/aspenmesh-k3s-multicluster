c11b1d87df3781d58cb893fa71ab7a028a75028a
Bart Van Bos <bartvanbos@gmail.com>
2021-05-18 19:12:59 +0200

    refactor WIP

--- install/rancher.sh
+++ install/rancher.sh
@@ -2,6 +2,7 @@
 
 
 if [[ $1 = "start" ]]; then
+  echo "sudo docker run -d --network=host --restart=always --name f5rancher -v ~/dockerhost-storage/rancher:/var/lib/rancher --privileged rancher/rancher:latest"
   sudo docker run -d \
     --network=host \
     --restart=always \
@@ -13,11 +14,15 @@ if [[ $1 = "start" ]]; then
 fi
 
 if [[ $1 = "stop" ]]; then
+  echo "sudo docker stop f5rancher"
   sudo docker stop f5rancher
+  echo "sudo docker rm f5rancher"
+  sudo docker rm f5rancher
   exit 0
 fi
 
 if [[ $1 = "logs" ]]; then
+  echo "sudo docker logs f5rancher"
   sudo docker logs f5rancher
   exit 0
 fi
